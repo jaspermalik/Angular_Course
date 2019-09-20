832050fe7db4da6253c071e75a376257594f527b
japsermalik <jaspermalik729@gmail.com>
2019-09-20 22:14:29 +0800

    RouterModule.forChild

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { ReactiveFormsModule } from '@angular/forms'
-import { RegisterComponent } from './components/register/register.component'
+import { RegisterComponent } from './components/register/register.component';
+import { UserRoutingModule } from './user-routing.module'
 
 @NgModule({
   declarations: [RegisterComponent],
-  imports: [CommonModule, ReactiveFormsModule]
+  imports: [CommonModule, ReactiveFormsModule, UserRoutingModule]
 })
 export class UserModule {}

f454537eb6d37126f65bb7d842ad58349c84acbd
japsermalik <jaspermalik729@gmail.com>
2019-09-20 21:58:16 +0800

    wildcard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router'
 import { PostComponent } from './modules/post/post.component'
 import { PostDetailsComponent } from './modules/post/components/post-details/post-details.component'
 import { RegisterComponent } from './modules/user/components/register/register.component'
+import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component'
 
 const routes: Routes = [
   { path: 'posts', component: PostComponent },
   { path: 'posts/:id', component: PostDetailsComponent },
   { path: 'register', component: RegisterComponent },
-  { path: '', redirectTo: '/posts', pathMatch: 'full' }
+  { path: '', redirectTo: '/posts', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
 ]
 
 @NgModule({

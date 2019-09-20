ce4efe0f20fd919d40a9acdbe12882ad7dda5bd7
japsermalik <jaspermalik729@gmail.com>
2019-09-20 22:45:41 +0800

    DemoModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
-
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { PostModule } from './modules/post/post.module'
 import { UserModule } from './modules/user/user.module'
 import { HeaderComponent } from './core/components/header/header.component'
-import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component'
+import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
+import { DemoModule } from './modules/demo/demo.module'
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, PageNotFoundComponent],
-  imports: [BrowserModule, PostModule, UserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    PostModule,
+    UserModule,
+    DemoModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })

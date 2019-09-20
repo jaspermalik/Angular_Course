2db8660510fecb625e83ab1b4945da2082cb67d5
japsermalik <jaspermalik729@gmail.com>
2019-09-20 22:08:09 +0800

    Routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { NgModule } from '@angular/core'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { PostModule } from './modules/post/post.module'
-import { UserModule } from './modules/user/user.module';
-import { HeaderComponent } from './core/components/header/header.component';
+import { UserModule } from './modules/user/user.module'
+import { HeaderComponent } from './core/components/header/header.component'
 import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component'
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, PageNotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, PostModule, UserModule],
+  imports: [BrowserModule, PostModule, UserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })

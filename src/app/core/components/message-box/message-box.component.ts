db044928d7c42cbd67cc8137cde81fafbd618e07
japsermalik <jaspermalik729@gmail.com>
2019-09-21 21:55:24 +0800

    清除二级路由

--- src/app/core/components/message-box/message-box.component.ts
+++ src/app/core/components/message-box/message-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-message-box',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message-box.component.css']
 })
 export class MessageBoxComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  closePopup() {
+    this.router.navigate([{ outlets: { popup: null } }])
   }
-
 }

89608e6d9718636881d224b73f524cd512676735
japsermalik <jaspermalik729@gmail.com>
2019-09-21 21:45:58 +0800

    name outlets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
 import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component'
+import { MessageBoxComponent } from './core/components/message-box/message-box.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '/posts', pathMatch: 'full' },
-  { path: '**', component: PageNotFoundComponent }
+  { path: '**', component: PageNotFoundComponent },
+  { path: 'message', component: MessageBoxComponent, outlet: 'popup' }
 ]
 
 @NgModule({
--- src/app/modules/demo/demo.module.ts
+++ src/app/modules/demo/demo.module.ts
@@ -1,16 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
 
-import { DemoRoutingModule } from './demo-routing.module';
-import { DemoComponent } from './demo.component';
-import { ChildRoutesComponent } from './child-routes/child-routes.component';
-
+import { DemoRoutingModule } from './demo-routing.module'
+import { DemoComponent } from './demo.component'
+import { ChildRoutesComponent } from './child-routes/child-routes.component'
 
 @NgModule({
   declarations: [DemoComponent, ChildRoutesComponent],
-  imports: [
-    CommonModule,
-    DemoRoutingModule
-  ]
+  imports: [CommonModule, DemoRoutingModule]
 })
-export class DemoModule { }
+export class DemoModule {}

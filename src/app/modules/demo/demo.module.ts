79da327b927c8eb9155da7e2969b67c9baf9079c
japsermalik <jaspermalik729@gmail.com>
2019-09-23 22:34:24 +0800

    create admin component

--- src/app/modules/demo/demo.module.ts
+++ src/app/modules/demo/demo.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common'
 import { DemoRoutingModule } from './demo-routing.module'
 import { DemoComponent } from './demo.component'
 import { ChildRoutesComponent } from './child-routes/child-routes.component';
-import { LoginComponent } from './login/login.component'
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component'
 
 @NgModule({
-  declarations: [DemoComponent, ChildRoutesComponent, LoginComponent],
+  declarations: [DemoComponent, ChildRoutesComponent, LoginComponent, AdminComponent],
   imports: [CommonModule, DemoRoutingModule]
 })
 export class DemoModule {}

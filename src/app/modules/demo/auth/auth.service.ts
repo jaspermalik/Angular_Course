113846fb97fd6d551c325bf0055925969cffd5bc
japsermalik <jaspermalik729@gmail.com>
2019-09-23 22:17:18 +0800

    login component && auth service

---
+++ src/app/modules/demo/auth/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  isLoggedIn = false
+  login() {
+    this.isLoggedIn = true
+    console.log('Logged in!')
+  }
+  logout() {
+    this.isLoggedIn = false
+    console.log('Logged out!')
+  }
+  constructor() {}
+}
--- src/app/modules/demo/demo-routing.module.ts
+++ src/app/modules/demo/demo-routing.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
 import { DemoComponent } from './demo.component'
 import { ChildRoutesComponent } from './child-routes/child-routes.component'
+import { LoginComponent } from './login/login.component'
 
 const routes: Routes = [
   {
     path: 'demo',
     component: DemoComponent,
-    children: [{ path: 'child-routes', component: ChildRoutesComponent }]
+    children: [
+      { path: 'child-routes', component: ChildRoutesComponent },
+      {
+        path: 'login',
+        component: LoginComponent
+      }
+    ]
   }
 ]
 

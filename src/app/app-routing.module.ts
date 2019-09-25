cad10a8f86ea0d95264bcf2f860588005f5b24e1
japsermalik <jaspermalik729@gmail.com>
2019-09-25 17:18:48 +0800

    custom preloading strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core'
-import { Routes, RouterModule, PreloadAllModules } from '@angular/router'
+import { Routes, RouterModule } from '@angular/router'
 import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component'
 import { MessageBoxComponent } from './core/components/message-box/message-box.component'
+import { SelectivePreloadingStrategyService } from './core/services/selective-preloading-strategy.service'
 
 const routes: Routes = [
   { path: '', redirectTo: '/posts', pathMatch: 'full' },
   {
     path: 'demo',
     loadChildren: () =>
-      import('./modules/demo/demo.module').then(module => module.DemoModule)
+      import('./modules/demo/demo.module').then(module => module.DemoModule),
+    data: {
+      preload: true
+    }
   },
   { path: '**', component: PageNotFoundComponent },
   { path: 'message', component: MessageBoxComponent, outlet: 'popup' }
@@ -16,7 +20,9 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: SelectivePreloadingStrategyService
+    })
   ],
   exports: [RouterModule]
 })
